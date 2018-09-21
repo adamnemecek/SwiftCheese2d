b08cddcdc74f5c4a6e01b8b4644fabcbdc7a182d
Nail Sharipov <n.sharipov@dlg.im>
2018-09-21 10:03:47 +0300

    project reconfiguration

--- Cheese2d/Cheese2d/Cheese2d.h
+++ Cheese2d/Cheese2d/Cheese2d.h
@@ -2,7 +2,7 @@
 //  Cheese2d.h
 //  Cheese2d
 //
-//  Created by Nail Sharipov on 18/09/2018.
+//  Created by Nail Sharipov on 21/09/2018.
 //  Copyright © 2018 Nail Sharipov. All rights reserved.
 //
 
